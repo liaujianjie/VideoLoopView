eee9a78cc0998c0005fd4dec2be58b19b5f6ae08
Liau Jian Jie <jianjie@buuuk.com>
2016-02-14 16:10:31 +0800

    Initial Commit

---
+++ LoopingVideoViewTests/LoopingVideoViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  LoopingVideoViewTests.swift
+//  LoopingVideoViewTests
+//
+//  Created by Jian Jie on 14/2/16.
+//  Copyright © 2016 Liau Jian Jie. All rights reserved.
+//
+
+import XCTest
+@testable import LoopingVideoView
+
+class LoopingVideoViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
